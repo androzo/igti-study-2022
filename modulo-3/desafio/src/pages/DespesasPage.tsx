54f7d58cfa8ca7aa3f3fa0a8adfc98088e3951b7
André Buchmann <androzo@gmail.com>
2022-03-11 21:05:24 -0300

    add reducer

--- modulo-3/desafio/src/pages/DespesasPage.tsx
+++ modulo-3/desafio/src/pages/DespesasPage.tsx
@@ -1,5 +1,5 @@
 import Container from "@material-ui/core/Container";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { getDespesasEndpoint, IDespesa } from "../services/api";
 import { useParams } from "react-router";
 import { ToastContainer, toast } from "react-toastify";
@@ -9,50 +9,113 @@ import { Link } from "react-router-dom";
 import DespesasHeader from "../components/DespesasHeader";
 import DespesasTab from "../components/DespesasTab";
 
+function sumDespesas(despesas: IDespesa[]) {
+  let sumTotalExpenses: number = 0;
+  despesas.map((despesa) => {
+    sumTotalExpenses = sumTotalExpenses + despesa.valor;
+    return despesa;
+  });
+  return sumTotalExpenses;
+}
+
+function parseAndSortDespesas(despesas: IDespesa[]) {
+  despesas.map((despesa) => {
+    despesa.dia = +despesa.dia;
+    return despesa;
+  });
+
+  return despesas.sort((a, b) => a.dia - b.dia);
+}
+
+function popError() {
+  return toast.error(
+    "Nenhuma despesa encontrada para esta data! Tente outra data",
+    {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    }
+  );
+}
+
+interface IDespesasPageState {
+  despesas: IDespesa[];
+  totalExpenses: number;
+  year: string;
+  month: string;
+}
+
+type IDespesasPageAction =
+  | {
+      type: "load";
+      payload: {
+        despesas: IDespesa[];
+      };
+    }
+  | {
+      type: "refreshYear";
+      payload: {
+        year: string;
+      };
+    }
+  | {
+      type: "refreshMonth";
+      payload: {
+        month: string;
+      };
+    };
+
+function reducer(
+  state: IDespesasPageState,
+  action: IDespesasPageAction
+): IDespesasPageState {
+  switch (action.type) {
+    case "load":
+      return {
+        ...state,
+        despesas: parseAndSortDespesas(action.payload.despesas),
+        totalExpenses: sumDespesas(action.payload.despesas),
+      };
+    case "refreshYear":
+      return { ...state, year: action.payload.year };
+    case "refreshMonth":
+      return { ...state, month: action.payload.month };
+
+    default:
+      return state;
+  }
+}
+
 export default function DespesasPage() {
   const params = useParams<{ year: string; month: string }>();
-  const [despesas, setDespesas] = useState<IDespesa[]>([]);
-  const [year, setYear] = useState(params.year);
-  const [month, setMonth] = useState(params.month);
-  const [totalExpenses, setTotalExpenses] = useState<number>(0);
 
-  const onYearChange = (value: string) => {
-    setYear(value);
-    return <Link to={`${year}-${value}`}></Link>;
+  const [state, dispatch] = useReducer(reducer, {
+    despesas: [],
+    totalExpenses: 0,
+    year: params.year,
+    month: params.month,
+  });
+
+  const { despesas, totalExpenses, year, month } = state;
+
+  const onYearChange = (year: string) => {
+    dispatch({ type: "refreshYear", payload: { year } });
+    return <Link to={`${year}-${month}`}></Link>;
   };
 
-  const onMonthChange = (value: string) => {
-    setMonth(value);
-    return <Link to={`${value}-${month}`}></Link>;
+  const onMonthChange = (month: string) => {
+    dispatch({ type: "refreshMonth", payload: { month } });
+    return <Link to={`${year}-${month}`}></Link>;
   };
-
-  function popError() {
-    return toast.error(
-      "Nenhuma despesa encontrada para esta data! Tente outra data",
-      {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      }
-    );
-  }
 
   useEffect(() => {
-    let sumTotalExpenses: number = 0;
-    getDespesasEndpoint(year, month).then((response) => {
-      response.map((despesa) => {
-        sumTotalExpenses = sumTotalExpenses + despesa.valor;
-        despesa.dia = +despesa.dia;
-        return true;
-      });
-      setDespesas(response.sort((a, b) => a.dia - b.dia));
-      setTotalExpenses(sumTotalExpenses);
-
-      if (response.length === 0) {
+    getDespesasEndpoint(year, month).then((despesas) => {
+      dispatch({ type: "load", payload: { despesas } });
+      if (despesas.length === 0) {
         popError();
       }
     });
