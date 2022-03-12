ab1bf515d0e12416fda677b8f8cdb66b82ce77fc
André Buchmann <androzo@gmail.com>
2022-03-12 13:29:03 -0300

    feat(Despesas): add reducer and custom hook

--- modulo-3/desafio/src/App.tsx
+++ modulo-3/desafio/src/App.tsx
@@ -7,7 +7,7 @@ import {
   Redirect,
 } from "react-router-dom";
 import { getUserEndpoint, IUser, signOutEndpoint } from "./services/api";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authContext } from "./authContext";
 import LoginPage from "./pages/LoginPage";
 
@@ -38,6 +38,52 @@ function App() {
     );
   } else {
     return <LoginPage onSignIn={setUser} />;
+  }
+}
+
+class App2 extends React.Component<{}, { user: IUser | null }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+  }
+
+  render() {
+    const { user } = this.state;
+
+    const setUser = (user: IUser) => {
+      this.setState({ user });
+    };
+
+    const onSignOut = () => {
+      signOutEndpoint();
+      this.setState({ user: null });
+    };
+
+    if (user) {
+      return (
+        <authContext.Provider value={{ user, onSignOut }}>
+          <Router>
+            <Switch>
+              <Route path="/despesas/:year-:month">
+                <DespesasPage />
+              </Route>
+              <Redirect to={{ pathname: "/despesas/2020-06" }}></Redirect>
+            </Switch>
+          </Router>
+        </authContext.Provider>
+      );
+    } else {
+      return <LoginPage onSignIn={setUser} />;
+    }
+  }
+
+  componentDidMount() {
+    getUserEndpoint().then(
+      (response) => this.setState({ user: response }),
+      () => this.setState({ user: null })
+    );
   }
 }
 
