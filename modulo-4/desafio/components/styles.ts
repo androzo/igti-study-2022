7a814bd40ae48d2069028c1942f500fdf3b6d23f
André Buchmann <androzo@gmail.com>
2022-03-26 22:06:42 -0300

    implementar search e paginação

--- modulo-4/desafio/components/styles.ts
+++ modulo-4/desafio/components/styles.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
+import Button from "@material-ui/core/Button";
 
-export const StyledButton = styled.button`
-  padding: 10px;
-  border-radius: 10%;
-  cursor: pointer;
-  margin-right: 10px;
+export const StyledButton = styled(Button)`
+  background-color: #6772e5;
+  color: #fff;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  padding: 7px 14px;
+  &:hover {
+    background-color: #5469d4;
+  }
 `;
--- modulo-4/desafio/pages/index.tsx
+++ modulo-4/desafio/pages/index.tsx
@@ -2,32 +2,58 @@ import Button from "@/components/Button";
 import { useEffect, useState } from "react";
 import { getDataFromApi, IExchange } from "../services/api";
 import { StyledHeader } from "./styles";
+import TextField from "@material-ui/core/TextField";
+import { Box } from "@material-ui/core";
+import styled from "styled-components";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+
+const StyledTextField = styled(TextField)`
+  margin-left: 10px;
+`;
 
 export default function Home() {
   const [data, setData] = useState<IExchange[]>([]);
+  const [filteredData, setFilteredData] = useState<IExchange[]>([]);
   const [pageIndex, setPageIndex] = useState(1);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     getDataFromApi(pageIndex).then(setData);
   }, [pageIndex]);
 
+  useEffect(() => {
+    const filtered = data.filter((item) =>
+      item.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredData(filtered);
+  }, [searchText, data]);
+
+  const AnteriorOnClick = () => {
+    setSearchText("");
+    pageIndex !== 1 ? setPageIndex(pageIndex - 1) : {};
+  };
+
+  const ProximoOnClick = () => {
+    setSearchText("");
+    data.length === 100 ? setPageIndex(pageIndex + 1) : {};
+  };
+
   return (
     <div>
       <StyledHeader>Desafio Modulo 4</StyledHeader>
-      <Button
-        name="Anterior"
-        onClick={() => {
-          pageIndex !== 1 ? setPageIndex(pageIndex - 1) : {};
-        }}
-      />
-      <Button
-        name="Próximo"
-        onClick={() => {
-          data.length === 100 ? setPageIndex(pageIndex + 1) : {};
-        }}
-      />
+
+      <Button name="Anterior" onClick={AnteriorOnClick} />
+      <Button name="Próximo" onClick={ProximoOnClick} />
+      <Box>
+        <StyledTextField
+          id="standard-basic"
+          label="Search here"
+          value={searchText}
+          onChange={(evt) => setSearchText(evt.target.value)}
+        />
+      </Box>
       <ul>
-        {data?.map((item: any) => (
+        {filteredData?.map((item: any) => (
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
